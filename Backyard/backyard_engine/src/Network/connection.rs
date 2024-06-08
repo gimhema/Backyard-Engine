e79e159dd3bb00fc6849c80ef24131a1f503550e
Kimmo <49871739+gimhema@users.noreply.github.com>
2024-06-08 19:53:58 +0900

    Update connection.rs

--- Backyard/backyard_engine/src/Network/connection.rs
+++ Backyard/backyard_engine/src/Network/connection.rs
@@ -6,8 +6,19 @@ use mio::Token;
 
 pub struct connection
 {
+    token: Token,
     id: i64,
     tcpStream: TcpStream
+}
+
+impl connection {
+    pub fn new(_token: Token, _id: i64, _stream: TcpStream) -> Self {
+        connection {
+            token : _token,
+            id : _id,
+            tcpStream : _stream
+        }
+    }
 }
 
 pub struct connection_handler {
@@ -25,12 +36,21 @@ impl connection_handler {
         }
     }
 
-    pub fn GetConnetionByToken(&mut self, token: Token) -> Option<&mut TcpStream>
+    pub fn get_connetion_by_token(&mut self, token: Token) -> Option<&mut TcpStream>
     {
         if let Some(connection) = self.connections.get_mut(&token) {
             Some(&mut connection.tcpStream)
         } else {
             None
         }
+    }
+
+    pub fn new_connection(&mut self, _tcpStream : TcpStream, _token: Token)
+    {
+        // Id 처리 로직 필요함
+        let _new_connection = connection::new(_token, 0, _tcpStream);
+
+        self.connections.insert(_token, _new_connection);
+        self.tokenIdMap.insert(0, _token);
     }
 }
