1f30ffd9333f2b31d452b61c2e909399e1fbb7fa
hema <hemakim0215@naver.com>
2024-06-10 22:15:18 +0900

    .

--- Backyard/backyard_engine/src/Network/server_send.rs
+++ Backyard/backyard_engine/src/Network/server_send.rs
@@ -1,6 +1,18 @@
 use super::server::*;
-
+use super::connection::*;
 
 impl server {
-    
+
+    pub fn send_message_to_all_conn(&mut self, msg : String) {
+
+    }
+
+    pub fn send_message_to_target(&mut self, target : i64, msg : String) {
+
+    }
+
+    pub fn send_message_to_group(&mut self, target_vec : Vec<i64>, msg : String) {
+
+    }
+
 }
