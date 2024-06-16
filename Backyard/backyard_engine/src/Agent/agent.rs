d2456df81461b1f6c9ef1655f1661495c6c1c6e3
hema <hemakim0215@naver.com>
2024-06-16 22:11:13 +0900

    .

--- Backyard/backyard_engine/src/Agent/agent.rs
+++ Backyard/backyard_engine/src/Agent/agent.rs
@@ -1,1 +1,13 @@
 use super::Agent;
+
+
+
+// It can be a async task
+pub trait AgentLoopManager {
+    fn Update();
+}
+
+// AgentAction is dependent on AgentLoopManager
+pub trait AgentAction {
+    fn Update();    
+}
