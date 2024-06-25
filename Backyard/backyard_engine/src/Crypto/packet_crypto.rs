4fc56ca152162d455c6bbb0529f40186e42ab468
hema <hemakim0215@naver.com>
2024-06-25 22:29:00 +0900

    .

--- Backyard/backyard_engine/src/Crypto/packet_crypto.rs
+++ Backyard/backyard_engine/src/Crypto/packet_crypto.rs
@@ -1,12 +1,36 @@
+use std::result;
+
 
 
 
 
 pub enum CryptMode {
     DEFAULT,
+    AES,
+    ChaCha20,
+    RSA
 }
 
 pub struct cryption_processor {
     mode : CryptMode
+}
+
+impl cryption_processor {
+    pub fn new(_mode : CryptMode) -> Self {
+        cryption_processor { mode: _mode }
+    }
+
+    pub fn encrypt(target : String) -> String {
+        let mut result = "";
+
+        return result.to_string();
+    }
+
+    pub fn decrypt(target : String) -> String {
+        let mut result = "";
+
+        return result.to_string();
+    }
+
 }
 
