53b4132e888f1bd1695f44db127d5c87ce0abf1b
hema <hemakim0215@naver.com>
2024-07-01 22:02:06 +0900

    .

--- Backyard/client_interface/Go/Player/src/backyard_player.go
+++ Backyard/client_interface/Go/Player/src/backyard_player.go
@@ -1,25 +1,11 @@
 package backyard_player
 
-type NetworkProtocol int
-
-const (
-	DEFAULT NetworkProtocol = iota //0
-	TCP     NetworkProtocol = iota //1
-	UDP     NetworkProtocol = iota //2
-)
-
-type NetworkSettingInfo struct {
-	protocol  NetworkProtocol
-	port      int
-	ipAddress string
-}
-
 type PlayerNetworkManager struct {
-	setting NetworkSettingInfo
+	socket_manager SocketManager
 }
 
-func (pnManager *PlayerNetworkManager) Init(_setting NetworkSettingInfo) {
-	pnManager.setting = _setting
+func (pnManager *PlayerNetworkManager) Init() {
+
 }
 
 func (pnManager PlayerNetworkManager) Run() {
