448ceb7a5917e27ba5efe1d6102e72f71c3a9cf0
hema <hemakim0215@naver.com>
2024-07-01 22:21:27 +0900

    .

--- Backyard/client_interface/Go/Player/src/backyard_socket.go
+++ Backyard/client_interface/Go/Player/src/backyard_socket.go
@@ -1,5 +1,10 @@
 package backyard_player
 
+import (
+	"fmt"
+	"net"
+)
+
 type NetworkProtocol int
 
 const (
@@ -9,10 +14,54 @@ const (
 )
 
 type BackyardSocket struct {
-	socket_protocol NetworkProtocol
-	some_socket     interface{}
-	port            int
-	ipAddress       string
+	protocol  NetworkProtocol
+	socket    interface{}
+	port      string
+	ipAddress string
+}
+
+func (bSocket *BackyardSocket) MakeSocket(_protocol NetworkProtocol, _port string, _ipAddress string) {
+
+	bSocket.protocol = _protocol
+
+	switch _protocol {
+	case TCP:
+		bSocket.ipAddress = _ipAddress
+		bSocket.port = _port
+		bSocket.BuildSocketTCP()
+	case UDP:
+		bSocket.ipAddress = _ipAddress
+		bSocket.port = _port
+		bSocket.BuildSocketUDP()
+	default:
+		fmt.Println("Unknown socket type")
+	}
+
+}
+
+func (bSocket *BackyardSocket) BuildSocketTCP() {
+	connet_info := bSocket.ipAddress + ":" + bSocket.port
+
+	_socket, err := net.Dial("tcp", connet_info)
+
+	if err != nil {
+		fmt.Println("Create Socket Error")
+	} else {
+		bSocket.socket = _socket
+	}
+
+}
+
+func (bSocket *BackyardSocket) BuildSocketUDP() {
+	connet_info := bSocket.ipAddress + ":" + bSocket.port
+
+	_socket, err := net.ResolveUDPAddr("udp", connet_info)
+
+	if err != nil {
+		fmt.Println("Create Socket Error")
+	} else {
+		bSocket.socket = _socket
+	}
 }
 
 type SocketManager struct {
