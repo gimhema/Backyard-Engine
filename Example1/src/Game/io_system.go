846bf208aa680021bc8758a9f7b9d75b24947cfe
Kimmo <49871739+gimhema@users.noreply.github.com>
2024-07-12 16:42:17 +0900

    Update io_system.go

--- Example1/src/Game/io_system.go
+++ Example1/src/Game/io_system.go
@@ -1,8 +1,19 @@
 package game
 
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
 type IOManager struct {
+	keys []ebiten.Key
 }
 
-func (ioManager IOManager) Wait() {
+func (ioManager *IOManager) Update() error {
+	ioManager.keys = inpututil.AppendPressedKeys(ioManager.keys[:0])
+	return nil
+}
+
+func (ioManager *IOManager) Wait() {
 
 }
