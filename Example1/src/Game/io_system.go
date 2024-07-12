626d6d15a78b063a3290cd74683efe75ecd64895
Kimmo <49871739+gimhema@users.noreply.github.com>
2024-07-12 17:08:43 +0900

    Update io_system.go

--- Example1/src/Game/io_system.go
+++ Example1/src/Game/io_system.go
@@ -1,6 +1,10 @@
 package game
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/eiannone/keyboard"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
@@ -9,11 +13,29 @@ type IOManager struct {
 	keys []ebiten.Key
 }
 
-func (ioManager *IOManager) Update() error {
+func (ioManager *IOManager) InputWait() error {
 	ioManager.keys = inpututil.AppendPressedKeys(ioManager.keys[:0])
 	return nil
 }
 
-func (ioManager *IOManager) Wait() {
+func (ioManager *IOManager) Run() {
 
+	if err := keyboard.Open(); err != nil {
+		log.Fatal(err)
+	}
+	defer keyboard.Close()
+
+	for {
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if key == keyboard.KeyEsc {
+			fmt.Println("Program Exit.")
+			break
+		}
+
+		fmt.Printf("Key : %c\n", char)
+	}
 }
