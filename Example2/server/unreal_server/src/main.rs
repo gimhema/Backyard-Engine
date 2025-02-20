c4fd9cc12a15f2cba014b5e5df5c9f2326e4b28d
kimmo <hemakim0215@naver.com>
2025-02-20 23:16:37 +0900

    .

--- Example2/server/unreal_server/src/main.rs
+++ Example2/server/unreal_server/src/main.rs
@@ -1,3 +1,53 @@
+
+#[macro_use]
+extern crate lazy_static;
+
+mod Agent;
+mod Event;
+mod Network;
+mod qsm;
+mod Crypto;
+mod Session;
+
+// User Custom
+mod UserLogic;
+
+// Core Logic
+mod Core;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Server Start");
+
+    Core::core::MainLogic();
+
+    /*
+    // Run TCP
+    let server_instance = Arc::clone(get_tcp_server_instance());
+    thread::spawn(move || {
+        get_tcp_server_instance().write().unwrap().run();
+    });
+
+    thread::spawn(move || {
+        // listen message . . .
+        loop 
+        {
+            if false == get_callback_msg_queue_instance().read().unwrap().empty() 
+            {
+                println!("Fetch Message . . .");
+                // pop message
+                let mut _game_msg = get_callback_msg_queue_instance().write().unwrap().pop();
+                let mut _targetToken = _game_msg.get_token();
+                let mut _send_msg = _game_msg.get_message();
+
+                get_connection_handler().write().unwrap().send_message_to_stream(_targetToken, _send_msg);
+                println!("Completed Send Message . . .");
+            }
+        }
+    });
+
+    loop {
+        thread::sleep(Duration::from_secs(1));
+    }
+    */
+
 }
