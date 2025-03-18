fc250c5d46036d466b67529e04195ad3f68b2f66
kimmo <hemakim0215@naver.com>
2025-03-18 23:42:41 +0900

    .

--- Example2/server/unreal_server/src/qsm/user_event/event_chat.rs
+++ Example2/server/unreal_server/src/qsm/user_event/event_chat.rs
@@ -13,6 +13,8 @@ pub fn CallBack_Chat(buffer: &[u8])
 
             println!("sender id : {}", sender);
             println!("chat content : {}", chat_content);
+
+            get_udp_server_instance().write().unwrap().send_message_to_all_conn(buffer);
         }
         Err(e) => {
             eprintln!("Failed to deserialize ChatMessage: {}", e);
