23d5a620de67c35fcd06ab5a63eb57afbdcfeb55
kimmo <hemakim0215@naver.com>
2025-03-21 00:52:44 +0900

    .

--- Example2/client/Unreal_EventHandler/MessageCaster.cpp
+++ Example2/client/Unreal_EventHandler/MessageCaster.cpp
@@ -1,6 +1,8 @@
 #include "MessageCaster.h"
 #include "../Messages/ExampleMessage.hpp"
-
+#include "../Unreal_QMessage_Event/QEvent_Chat.hpp"
+#include "../Unreal_QMessage_Event/QEvent_NewPlayer.hpp"
+#include "../Unreal_QMessage_Event/QEvent_PlayerMovement.hpp"
 
 MessageCaster::MessageCaster()
 {
@@ -29,4 +31,17 @@ void MessageCaster::HandleMessage(const std::vector<uint8_t>& buffer)
     BaseMessage base_message = BaseMessage::deserialize(buffer);
 
     
+}
+
+void MessageCaster::InitEventMap()
+{
+    dispatcher.registerFunction(QFunctionType::CHAT, std::make_unique<QEvent_Chat>());
+    dispatcher.registerFunction(QFunctionType::NEW_PLAYER, std::make_unique<QEvent_NewPlayer>());
+    dispatcher.registerFunction(QFunctionType::PLAYER_MOVEMENT, std::make_unique<QEvent_PlayerMovement>());
+
+}
+
+void MessageCaster::ExecFunc(int fid, const std::vector<uint8_t>& buffer)
+{
+    dispatcher.execute(static_cast<QFunctionType>(fid), buffer);
 }
--- Example2/client/Unreal_EventHandler/MessageCaster.h
+++ Example2/client/Unreal_EventHandler/MessageCaster.h
@@ -5,6 +5,9 @@
 #include <cstring>  // memcpy
 #include <cstdint>  // uint32_t, uint64_t
 #include <stdexcept> // std::runtime_error
+#include <memory>
+#include <unordered_map>
+
 #include "MessageEnum.hpp"
 #include "../Unreal_QMessage_Event/QEvent_Base.h"
 
@@ -32,6 +35,37 @@ struct BaseMessage {
 };
 #pragma pack(pop)
 
+
+class FunctionDispatcher {
+    public:
+        using FunctionPtr = std::unique_ptr<IFunction>;
+    
+        void registerFunction(FunctionType type, FunctionPtr func) {
+            functions_.emplace(type, std::move(func));
+        }
+    
+        void execute(FunctionType type) const {
+            auto it = functions_.find(type);
+            if (it != functions_.end()) {
+                (*it->second)(); // 기본 실행
+            } else {
+                std::cerr << "No function registered for type: " << toInt(type) << std::endl;
+            }
+        }
+    
+        void execute(FunctionType type, const std::vector<uint8_t>& data) const {
+            auto it = functions_.find(type);
+            if (it != functions_.end()) {
+                (*it->second)(data); // 데이터 전달 실행
+            } else {
+                std::cerr << "No function registered for type: " << toInt(type) << std::endl;
+            }
+        }
+    
+    private:
+        std::unordered_map<FunctionType, FunctionPtr> functions_;
+    };
+
 class MessageCaster
 {
 public:
@@ -40,7 +74,7 @@ public:
 
 private:
     int MAX_BUUFER_SIZE = 2048;
-
+    FunctionDispatcher f_Dispatcher;
 
 public:
     void RecvPostProcess();
@@ -50,7 +84,7 @@ public:
     // Custom
 public:
     void InitEventMap();
-    void ExecFunc();
+    void ExecFunc(int fid, const std::vector<uint8_t>& buffer);
 
 };
 
