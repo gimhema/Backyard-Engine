d976e992d7c1ab61a94bd929206f3dfba0cb0969
Kimmo <49871739+gimhema@users.noreply.github.com>
2025-03-27 00:09:43 +0900

    Create TCPSocketListener.h

---
+++ Example2/client/TCPSocketListener/TCPSocketListener.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Sockets.h"
+#include "SocketSubsystem.h"
+#include "IPAddress.h"
+#include "HAL/Runnable.h"
+#include "HAL/RunnableThread.h"
+/**
+ * 
+ */
+class VOIDESCAPE_API FTCPSocketListener : public FRunnable
+{
+private:
+    FSocket* ServerSocket;
+    FSocket* ClientSocket;
+    FRunnableThread* Thread;
+    bool bRunThread;
+    const int32 BufferSize = 4096;
+
+public:
+    FTCPSocketListener();
+    virtual ~FTCPSocketListener();
+
+    bool StartTCPListener(int32 Port);
+    void StopTCPListener();
+
+private:
+    virtual uint32 Run() override;
+    void ReceiveData();
+};
