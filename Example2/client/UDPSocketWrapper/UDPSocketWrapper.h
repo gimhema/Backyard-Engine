753b0ab84f065e683da91aa3b66e02e139837fe0
Kimmo <49871739+gimhema@users.noreply.github.com>
2025-03-27 00:08:24 +0900

    Create UDPSocketWrapper.h

---
+++ Example2/client/UDPSocketWrapper/UDPSocketWrapper.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Sockets.h"
+#include "SocketSubsystem.h"
+#include "IPAddress.h"
+#include "HAL/Runnable.h"
+#include "HAL/RunnableThread.h"
+
+/**
+ * 
+ */
+
+class VOIDESCAPE_API FUDPSocketWrapper : public FRunnable
+{
+
+private:
+    FSocket* UdpSocket;
+    TSharedPtr<FInternetAddr> RemoteAddress;
+    FRunnableThread* Thread;
+    bool bRunThread;
+    const int32 BufferSize = 2048; // 2KB 버퍼
+
+public:
+    FUDPSocketWrapper();
+    virtual ~FUDPSocketWrapper();
+
+    void SendMessage(const FString& Message, const FString& TargetIP, int32 TargetPort);
+    void StopReceiving();
+
+private:
+    virtual uint32 Run() override;
+};
