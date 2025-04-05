426d65b34f548b982fa86daf425778fa137cc203
kimmo <hemakim0215@naver.com>
2025-04-05 11:37:09 +0900

    .

--- Example2/server/unreal_server/src/GameLogic/game_geometry.rs
+++ Example2/server/unreal_server/src/GameLogic/game_geometry.rs
@@ -1,13 +1,31 @@
 use std::fmt::Pointer;
 
-
-
 pub struct Rotation
 {
     q_x : f64,
     q_y : f64,
     q_z : f64,
-    f_w : f64
+    q_w : f64
+}
+
+impl Rotation
+{
+    pub fn new_zero() -> Self {
+        return Rotation { q_x: 0.0, q_y: 0.0, q_z: 0.0, q_w: 0.0 }
+    }
+
+    pub fn new(_x : f64, _y : f64, _z : f64, _w : f64) -> Self {
+        return Rotation { q_x: _x, q_y: _y, q_z: _z, q_w: _w }
+    }
+
+    pub fn set_x(&mut self, _x : f64) { self.q_x = _x; }
+    pub fn set_y(&mut self, _y : f64) { self.q_y = _y; }
+    pub fn set_z(&mut self, _z : f64) { self.q_z = _z; }
+    pub fn set_w(&mut self, _w : f64) { self.q_w = _w; }
+    pub fn get_x(&self) -> f64 {return self.q_x}
+    pub fn get_y(&self) -> f64 {return self.q_y}
+    pub fn get_z(&self) -> f64 {return self.q_z}
+    pub fn get_w(&self) -> f64 {return self.q_w}
 }
 
 pub struct Position
@@ -15,6 +33,23 @@ pub struct Position
     x : f64,
     y : f64,
     z : f64
+}
+
+impl Position {
+    pub fn new_zero() -> Self {
+        return Position { x: 0.0, y: 0.0, z: 0.0 }
+    }
+
+    pub fn new(_x : f64, _y : f64, _z : f64, _w : f64) -> Self {
+        return Position { x: _x, y: _y, z: _z}
+    }
+
+    pub fn set_x(&mut self, _x : f64) { self.x = _x; }
+    pub fn set_y(&mut self, _y : f64) { self.y = _y; }
+    pub fn set_z(&mut self, _z : f64) { self.z = _z; }
+    pub fn get_x(&self) -> f64 {return self.x}
+    pub fn get_y(&self) -> f64 {return self.y}
+    pub fn get_z(&self) -> f64 {return self.z}
 }
 
 pub struct Transform
