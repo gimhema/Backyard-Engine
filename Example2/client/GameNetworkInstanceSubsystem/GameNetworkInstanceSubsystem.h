a34d4764114e9b28f345a1b224ca94ac7781e90b
Kimmo <49871739+gimhema@users.noreply.github.com>
2025-04-07 22:51:17 +0900

    Create GameNetworkInstanceSubsystem.h

---
+++ Example2/client/GameNetworkInstanceSubsystem/GameNetworkInstanceSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "GameNetworkInstanceSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOIDESCAPE_API UGameNetworkInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
