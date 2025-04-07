aeb876aa10d50856dd13a518b363e238b62e44eb
Kimmo <49871739+gimhema@users.noreply.github.com>
2025-04-07 23:12:06 +0900

    Update GameNetworkInstanceSubsystem.cpp

--- Example2/client/GameNetworkInstanceSubsystem/GameNetworkInstanceSubsystem.cpp
+++ Example2/client/GameNetworkInstanceSubsystem/GameNetworkInstanceSubsystem.cpp
@@ -1,5 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "ServerMessageSubsystem.h"
+#include "GameNetworkInstanceSubsystem.h"
 
+void UGameNetworkInstanceSubsystem::RegisterHandler(FName MessageType, FMessageHandler Handler)
+{
+    HandlerMap.Add(MessageType, Handler);
+}
+
+void UGameNetworkInstanceSubsystem::DispatchMessage(FName MessageType, const TArray<uint8>& Payload)
+{
+    if (FMessageHandler* Handler = HandlerMap.Find(MessageType))
+    {
+        Handler->ExecuteIfBound(Payload);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("No handler for message type: %s"), *MessageType.ToString());
+    }
+}
