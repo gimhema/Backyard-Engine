7fca55914e6aa27258253186950daa340b14a6b5
kimmo <hemakim0215@naver.com>
2025-04-16 22:18:21 +0900

    .

--- Example2/server/unreal_server/src/qsm/user_event/event_make_account.rs
+++ Example2/server/unreal_server/src/qsm/user_event/event_make_account.rs
@@ -1,5 +1,6 @@
 use crate::get_tcp_server_instance;
 use crate::qsm::user_message::message_make_account::{self, MakeAccount};
+use crate::qsm::user_message::message_verify_account::{self, VerifyAccount};
 use crate::GameLogic::game_player::{get_ve_char_manager_instance, VECharcater};
 
 use super::GameLogic::*;
@@ -7,11 +8,23 @@ use super::GameLogic::*;
 pub fn CallBack_MakeAccount(buffer: &[u8])
 {
     match MakeAccount::deserialize(buffer) {
-        Ok(new_player_message) => {
+        Ok(make_account_message) => {
 
         }
         Err(e)=>{
-            eprintln!("Failed to deserialize MovementMessage: {}", e);
+            eprintln!("Failed to deserialize MakeAccount: {}", e);
+        }
+    }
+}
+
+pub fn CallBack_VerifyAccount(buffer: &[u8])
+{
+    match VerifyAccount::deserialize(buffer) {
+        Ok(verify_account_message) => {
+
+        }
+        Err(e) => {
+            eprintln!("Failed to deserialize VerifyAccount: {}", e);
         }
     }
 }
--- Example2/server/unreal_server/src/qsm/user_message/mod.rs
+++ Example2/server/unreal_server/src/qsm/user_message/mod.rs
@@ -2,3 +2,4 @@ pub mod message_chat;
 pub mod message_movement;
 pub mod message_new_player;
 pub mod message_make_account;
+pub mod message_verify_account;
