f36666990772a0f19a0fba9213f11ee8c5f68ae3
kimmo <hemakim0215@naver.com>
2025-05-05 12:37:35 +0900

    create game logic main

---
+++ Example2/server/unreal_server/src/GameLogic/game_logic_main.rs
@@ -0,0 +1,35 @@
+// game_logic.rs
+
+use crossbeam::queue::SegQueue;
+use std::sync::Arc;
+
+#[derive(Debug)]
+pub enum Command {
+    Move { entity_id: u32, dx: f32, dy: f32 },
+    Shoot { entity_id: u32 },
+}
+
+pub struct GameLogic {
+    pub command_queue: Arc<SegQueue<Command>>,
+}
+
+impl GameLogic {
+    pub fn new() -> Self {
+        GameLogic {
+            command_queue: Arc::new(SegQueue::new()),
+        }
+    }
+
+    pub fn process_commands(&self) {
+        while let Some(cmd) = self.command_queue.pop() {
+            match cmd {
+                Command::Move { entity_id, dx, dy } => {
+                    println!("Moving entity {} by {}, {}", entity_id, dx, dy);
+                }
+                Command::Shoot { entity_id } => {
+                    println!("Entity {} shoots!", entity_id);
+                }
+            }
+        }
+    }
+}
--- Example2/server/unreal_server/src/GameLogic/mod.rs
+++ Example2/server/unreal_server/src/GameLogic/mod.rs
@@ -2,5 +2,6 @@
 pub mod game_player;
 pub mod game_geometry;
 pub mod game_ecs;
+pub mod game_logic_main;
 
 use super::Network;
