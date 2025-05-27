c619b49f0bf6e2afb40ed053d1ba4e898e92a2eb
kimmo <hemakim0215@naver.com>
2025-05-27 23:36:57 +0900

    .

--- Example2/server/unreal_server/src/GameLogic/game_logic_action.rs
+++ Example2/server/unreal_server/src/GameLogic/game_logic_action.rs
@@ -1,13 +1,6 @@
 use super::{game_geometry::{Position, Rotation, Transform}, game_logic_main::*};
 
 
-pub fn push_command_create(entity_id: u32) {
-    let logic = G_GAME_LOGIC.lock().unwrap();
-    logic.command_queue.push(Command::Create {
-        entity_id
-    });    
-}
-
 pub fn do_command_create(_command: Command) {
     if let Command::Create { entity_id } = _command {
         println!("Create entity {}", entity_id);
@@ -17,10 +10,6 @@ pub fn do_command_create(_command: Command) {
         let mut game_logic = G_GAME_LOGIC.lock().unwrap();
 
         game_logic.game_world.create_entity(entity_id);
-
-        // if let Some(world) = game_logic.get_world_mut(0) {
-        //     world.create_entity(entity_id);
-        // }
     }
 }
 
@@ -33,31 +22,11 @@ pub fn do_command_delete(_command: Command) {
         let mut game_logic = G_GAME_LOGIC.lock().unwrap();
  
         game_logic.game_world.delete_entity(entity_id);
-        // if let Some(world) = game_logic.get_world_mut(0) {
-        //     world.delete_entity(entity_id);
-        // }
     }
 }
 
-// pub fn push_command_move(entity_id: u32, loc_x : f32, loc_y : f32, loc_z : f32,roll : f32, pitch : f32,yaw : f32) {
-//     let logic = G_GAME_LOGIC.lock().unwrap();
-//     logic.command_queue.push(Command::Move {
-//         entity_id,
-//         loc_x : loc_x,
-//         loc_y : loc_y,
-//         loc_z : loc_z,
-//         roll : roll,
-//         pitch : pitch,
-//         yaw : yaw    
-//     });
-// }
-
 pub fn do_command_move(_command : Command) {
     if let Command::Move { entity_id, loc_x, loc_y, loc_z, q_x, q_y, q_z, q_w } = _command {
-//        println!("Moving entity {} by {}, {}", entity_id, dx, dy);
-        // 추후에 월드 ID를 받아야함
-
-        // 우선 main 월드로 고정
 
         let mut game_logic = G_GAME_LOGIC.lock().unwrap();
 
@@ -66,8 +35,5 @@ pub fn do_command_move(_command : Command) {
             Rotation::new(q_x as f64, q_y as f64, q_z as f64, q_w as f64));
 
         game_logic.game_world.update_movement(entity_id, update_mov);
-        // if let Some(world) = game_logic.get_world_mut(0) {
-        //     world.update_movement(entity_id, update_mov);
-        // }
     }
 }
