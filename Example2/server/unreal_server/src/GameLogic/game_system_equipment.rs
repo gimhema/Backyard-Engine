2bb5b506d326cd13522b6f9b5005ea32cbca821b
kimmo <hemakim0215@naver.com>
2025-06-14 10:53:02 +0900

    .

--- Example2/server/unreal_server/src/GameLogic/game_system_equipment.rs
+++ Example2/server/unreal_server/src/GameLogic/game_system_equipment.rs
@@ -3,26 +3,34 @@ use std::vec;
 
 pub mod battle_interface
 {
-    pub fn damage_action_player_to_player(attack_id : i64, defense_id : i64, damage : i64)
+    pub fn damage_action_player_to_player(attack_id : i64, target_id : i64, damage : i64)
     {
-        let mut reduced_damage = calc_endurance(defense_id, damage);
-
-        hit_process(defense_id, reduced_damage);        
+        damage_common_action(target_id, damage);
     }
 
-    pub fn damage_action_in_range(defense_id_vec : Vec<i64>, damage : i64)
+    pub fn damage_action_in_range(target_id_vec : Vec<i64>, damage : i64)
     {
-        
+        for victims in target_id_vec {
+            damage_common_action(victims, damage);
+        }
     }
 
-    pub fn calc_endurance(defense_id : i64, damage : i64) -> i64
+    pub fn damage_common_action(target_id : i64, damage : i64) 
     {
+        let mut reduced_damage = calc_endurance(target_id, damage);
+
+        hit_process(target_id, reduced_damage);        
+    }
+
+    pub fn calc_endurance(target_id : i64, damage : i64) -> i64
+    {
+        // id를 통해 캐릭터의 방어력을 검사하고 데미지를 감쇠
         return 0
     }
 
-    pub fn hit_process(victim_id : i64, damage : i64) 
+    pub fn hit_process(target_id : i64, damage : i64) 
     {
-
+        // id를 통해 실질적으로 데미지를 적용하여 캐릭터의 체력 감소
     }
 }
 
