79c35b3c30c10e2980c546d7a3d1673a47ae2a73
Kimmo <49871739+gimhema@users.noreply.github.com>
2025-07-10 18:39:21 +0900

    Update connection.rs

--- Example2/server/unreal_server/src/Network/connection.rs
+++ Example2/server/unreal_server/src/Network/connection.rs
@@ -5,3 +5,11 @@ use mio::Token;
 use std::io::{self, Read, Write};
 use std::net::IpAddr; // SocketAddr 대신 IpAddr만 사용하는 경우
 use std::sync::{RwLock, Arc, RwLockReadGuard};
+
+// --- 클라이언트 연결 구조체 ---
+pub struct ClientConnection {
+    pub stream: TcpStream,
+    pub addr: SocketAddr,
+    pub write_queue: Arc<Mutex<Vec<u8>>>,
+    pub is_udp_client: bool, // UDP 클라이언트인지 여부 (TCP와 UDP 연결을 구분)
+}
