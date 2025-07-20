d52ddd488ca2f55218d79d468f56fdf4f6b24a4d
kimmo <hemakim0215@naver.com>
2025-07-20 11:12:36 +0900

    .

--- Example2/server/unreal_server/src/Core/core.rs
+++ Example2/server/unreal_server/src/Core/core.rs
@@ -9,11 +9,28 @@ use std::sync::Arc;
 use std::time::Duration;
 
 pub enum ServerMode {
+    NONE,
     GARDNER,
     GAME_STATUS_SERVICE,
     GAME_AUTH_SERVICE,
     GAME_CONNECTION_SERVICE,
     GAME_DB_AGENT
+}
+
+fn get_first_word<'a>(s: &'a str) -> &'a str {
+    s.split_whitespace().next().unwrap_or("")
+}
+
+pub fn set_mode_by_prefix(argv: String) -> ServerMode {
+    let first_word = get_first_word(&argv);
+    match first_word.to_lowercase().as_str() {
+        "gardner" => ServerMode::GARDNER,
+        "gss" => ServerMode::GAME_STATUS_SERVICE,
+        "gas" => ServerMode::GAME_AUTH_SERVICE,
+        "gcs" => ServerMode::GAME_CONNECTION_SERVICE,
+        "gda" => ServerMode::GAME_DB_AGENT,
+        _ => ServerMode::NONE,
+    }
 }
 
 pub fn read_server_option(mut argv: Vec<String>) {
