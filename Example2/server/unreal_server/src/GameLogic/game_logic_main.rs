38dbc3df708c6b505f7664797a551f432403fd6d
kimmo <hemakim0215@naver.com>
2025-09-12 11:08:51 +0900

    .

--- Example2/server/unreal_server/src/GameLogic/game_logic_main.rs
+++ Example2/server/unreal_server/src/GameLogic/game_logic_main.rs
@@ -1,25 +1,30 @@
 use crossbeam::queue::SegQueue;
-use std::sync::Arc;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
+use std::net::SocketAddr;
 
 use super::game_ecs::*;
 use super::game_logic_action::*;
+use std::collections::HashMap;
 
-use std::collections::{HashMap};
+// 추가: 전송 인터페이스
+use crate::Network::net_tx::NetSender;
 
 
 #[derive(Debug)]
 pub enum Command {
     Create { entity_id: u32 },
-    Delete {entity_id: u32},
+    Delete { entity_id: u32 },
     Move { entity_id: u32, loc_x: f32, loc_y: f32, loc_z: f32, q_x: f32, q_y: f32, q_z: f32, q_w: f32 },
     Shoot { entity_id: u32 },
+
+    NetSendUdp { addr: SocketAddr, payload: Vec<u8> },
 }
 
 pub struct GameLogicMain {
     pub command_queue: Arc<SegQueue<Command>>,
-//    pub world_container : HashMap<i64, World>
     pub game_world : World,
+
+    net_tx: Option<Arc<dyn NetSender>>,
 }
 
 impl GameLogicMain {
@@ -27,7 +32,12 @@ impl GameLogicMain {
         GameLogicMain {
             game_world : World::new(),
             command_queue: Arc::new(SegQueue::new()),
+            net_tx: None,
         }
+    }
+
+    pub fn set_net_sender(&mut self, tx: Arc<dyn NetSender>) {
+        self.net_tx = Some(tx);
     }
 
     pub fn world_create(&mut self) {
@@ -57,8 +67,25 @@ impl GameLogicMain {
                 }
                 Command::Shoot { entity_id } => {
                     println!("Entity {} shoots!", entity_id);
+                    // Example: Send a UDP message when an entity shoots
+                    // self.try_send_udp(addr, payload);
+                }
+                Command::NetSendUdp { addr, payload } => {
+                    self.try_send_udp(addr, payload);
                 }
             }
+        }
+    }
+
+    #[inline]
+    fn try_send_udp(&self, addr: SocketAddr, payload: Vec<u8>) {
+        if let Some(tx) = &self.net_tx {
+            if let Err(_) = tx.send_udp(addr, payload) {
+                // 큐가 가득 찬 경우 로깅/백프레셔 전략 택1
+                eprintln!("[GameLogic] UDP queue is full; dropping packet to {}", addr);
+            }
+        } else {
+            eprintln!("[GameLogic] NetSender not set; cannot send UDP");
         }
     }
 }
