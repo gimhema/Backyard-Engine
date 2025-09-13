862dd09608b810c85192a75af1af8ce37d834841
kimmo <hemakim0215@naver.com>
2025-09-13 10:13:03 +0900

    .

--- Backyard/backyard_engine/src/Core/core.rs
+++ Backyard/backyard_engine/src/Core/core.rs
@@ -1,51 +1,50 @@
 use super::Core;
 
-use super::Network::server::get_tcp_server_instance;
 use super::Network::message_queue::get_callback_msg_queue_instance;
-use super::Network::server_common::get_user_connection_info;
-use super::Network::server_common::get_connection_handler;
+// use super::Network::server_common::get_user_connection_info;
+// use super::Network::server_common::get_connection_handler;
 
 use std::thread;
 use std::sync::Arc;
 use std::time::Duration;
 
-pub fn SpawnWorkerMessageAction() {
-    let server_instance = Arc::clone(get_tcp_server_instance());
-    thread::spawn(move || {
-        get_tcp_server_instance().write().unwrap().run();
-    });
+
+#[derive(Debug, Clone, Copy)]
+pub enum ServerMode {
+    NONE,
+    GARDNER,
+    GAME_STATUS_SERVICE,
+    GAME_AUTH_SERVICE,
+    GAME_CONNECTION_SERVICE,
+    GAME_DB_AGENT
 }
 
-pub fn SpawnWorkerServerRun() {
-    thread::spawn(move || {
-        // listen message . . .
-        loop 
-        {
-            if false == get_callback_msg_queue_instance().read().unwrap().empty() 
-            {
-                println!("Fetch Message . . .");
-                // pop message
-                let mut _game_msg = get_callback_msg_queue_instance().write().unwrap().pop();
-                let mut _targetToken = _game_msg.get_token();
-                let mut _send_msg = _game_msg.get_message();
-
-                get_connection_handler().write().unwrap().send_message_to_stream(_targetToken, _send_msg);
-                println!("Completed Send Message . . .");
-            }
-        }
-    });
-
+fn get_first_word<'a>(s: &'a str) -> &'a str {
+    s.split_whitespace().next().unwrap_or("")
 }
 
-
-pub fn MainLogic() {
-
-    SpawnWorkerServerRun();
-
-    SpawnWorkerMessageAction();
-
-    loop {
-        thread::sleep(Duration::from_secs(1));
+pub fn set_mode_by_prefix(argv: String) -> ServerMode {
+    let first_word = get_first_word(&argv);
+    match first_word.to_lowercase().as_str() {
+        "gardner" => ServerMode::GARDNER,
+        "gss" => ServerMode::GAME_STATUS_SERVICE,
+        "gas" => ServerMode::GAME_AUTH_SERVICE,
+        "gcs" => ServerMode::GAME_CONNECTION_SERVICE,
+        "gda" => ServerMode::GAME_DB_AGENT,
+        _ => ServerMode::NONE,
     }
 }
 
+pub fn read_server_option(mut argv: Vec<String>) -> ServerMode {
+    println!("Entering run function with arguments: {:?}", argv);
+
+           if argv.len() < 2 {
+            println!("Insufficient arguments.");
+            return ServerMode::NONE
+        }
+
+    
+        let mode = set_mode_by_prefix(argv[1].clone());
+
+        return mode
+}
