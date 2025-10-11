1a7a695bba19ff4c0d27cc10ed19438b19e75684
kimmo <hemakim0215@naver.com>
2025-10-11 11:26:28 +0900

    .

--- Example2/server/unreal_server/src/GameLogic/game_logic_action_combat.rs
+++ Example2/server/unreal_server/src/GameLogic/game_logic_action_combat.rs
@@ -1,14 +1,27 @@
 use crate::GameLogicMain;
 use crate::Command;
 
-
+// Command Actions
 impl GameLogicMain {
 
     pub fn do_command_shoot(&mut self, _command : Command) {
     if let Command::Shoot { entity_id, target_id, damage } = _command {
         println!("Entity {} shoot!", entity_id);
 
-        // . . .
+            self.damage_action(entity_id, target_id, damage);
+
         }
     }
+}
+
+// Combat Actions
+impl GameLogicMain {
+    
+    pub fn damage_action(&mut self, attacker_id: u32, target_id: u32, damage: u32) {
+        println!("Entity {} damage {} to Entity {}", attacker_id, damage, target_id);
+
+
+    }
+
+
 }
