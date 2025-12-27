375b802ca1e05bb8b6996402b7b6709f97182c62
kimmo <hemakim0215@naver.com>
2025-12-27 10:20:20 +0900

    remove unused logic in main

--- Backyard/backyard_engine/src/main.rs
+++ Backyard/backyard_engine/src/main.rs
@@ -1,40 +1,17 @@
-// use qsm::qsm::get_event_handler;
-// use Network::server_datagram::get_udp_server_instance;
-
-
 #[macro_use]
 extern crate lazy_static;
 
 mod Agent;
+mod Core;
 mod Event;
 mod Network;
 mod qsm;
-mod Crypto;
-mod Session;
 
+use std::io;
+use crate::Network::server::Server;
 
-// User Custom
-mod UserLogic;
-
-// Core Logic
-mod Core;
-
-use std::sync::Arc;
-use std::thread;
-use std::sync::Mutex;
-use std::time::{Duration, Instant};
-// use tokio::time::Duration;
-use crate::Network::message_queue::*;
-use crate::Network::server::*;
-use tokio::io;
-use mio::Token;
-
-// // --- 메인 함수 ---
 fn main() -> io::Result<()> {
-
-    // 서버 인스턴스 생성
     let mut server = Server::new("127.0.0.1:8080", "127.0.0.1:8082")?;
-    // 서버 시작
     server.start()?;
     Ok(())
 }
